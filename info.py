a07007e9b45a34d0e13155cd0c0609ceffd31f01
vasanthakumar200612 <110110352+vasanthakumar200612@users.noreply.github.com>
2024-05-01 12:11:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://vasanthakumar200612:oorrwuyoui@autofilterbot.zidghl6.mongodb.net/?retryWrites=true&w=majority&appName=AutoFilterBot")
+DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://vasanthakumar200612:oorrwuyoui@autofilterbot.zidghl6.mongodb.net/")
 DATABASE_NAME = environ.get('DATABASE_NAME', "AutoFilterBot")
 FILE_DB_URL = environ.get("FILE_DB_URL", DATABASE_URL)
 FILE_DB_NAME = environ.get("FILE_DB_NAME", DATABASE_NAME)
